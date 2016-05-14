97ac15eba97bf390ac75ea4845f4e6c6ec51f07e
sdkraemer <tfcmaster9@gmail.com>
2016-05-14 11:48:22 -0700

    Added marker to most recent position

--- public/scripts/pirate.js
+++ public/scripts/pirate.js
@@ -7,10 +7,11 @@ var MapModule = (function(){
 	var initialize = function(mapElem, locations){
 		_locations = locations;
 		_googleMapsLatLng = _getGoogleMapsLatLng();
-		console.dir(_locations);
+
 		_initializeMap(mapElem);
 		drawPolyLine(_googleMapsLatLng);
 		centerMap(_googleMapsLatLng);
+		addMarkerAtMostRecentPosition();
 	};
 
 	var _getGoogleMapsLatLng = function(){
@@ -19,7 +20,30 @@ var MapModule = (function(){
 			googleMapsLatLng.push(new google.maps.LatLng(loc.lat, loc.long));
 		});
 		return googleMapsLatLng;
-	}
+	};
+
+	var addMarkerAtMostRecentPosition = function(){
+		var mostRecentPosition = _googleMapsLatLng[_googleMapsLatLng.length-1];
+		addMarker(mostRecentPosition, "We\'re here right now!");
+	};
+
+	var addMarker = function(googleMapsLatLng, content){
+		var marker = new google.maps.Marker({
+	    position: googleMapsLatLng,
+	    map: _map,
+	    title: 'We\'re here right now!'
+	  });
+
+		google.maps.event.addListener(marker, 'click', (function(marker) {
+			var infowindow = new google.maps.InfoWindow({
+			      maxWidth: 160
+			});
+    	return function() {
+      		infowindow.setContent(content);
+      		infowindow.open(_map, marker);
+      	}
+    })(marker));
+	};
 
 	var _initializeMap = function(mapElem){
 		_map = new google.maps.Map(mapElem, {
