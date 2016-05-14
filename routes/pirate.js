b6ba8d1172e257664570edf4eda0beaa54e16fcc
sdkraemer <tfcmaster9@gmail.com>
2016-05-14 11:33:59 -0700

    Updated map javascript to use revealing module pattern. Consolidated passed view variables to one json view object.

--- routes/pirate.js
+++ routes/pirate.js
@@ -6,15 +6,16 @@ var router = express.Router();
 
 /* GET user profile. */
 router.get('/', ensureLoggedIn, function(req, res, next) {
-  var userId = "87c6673b-ca53-440f-80ea-9c581caa6c1b";
-  console.log("user: %j", req.user);
   getUserObj(req.user.token, function(ninjas){
     var firstNinjaId = getFirstNinja(ninjas);
-    console.log("firstNinjaId: "+firstNinjaId);
     getLocationsFromNinja(req.user.token, firstNinjaId, function(locations){
-      console.log(locations);
       var stringifiedLocations = JSON.stringify(locations);
-      res.render('pirate', { user: req.user, locations: locations, ninjas: ninjas, stringifiedLocations: stringifiedLocations });
+      var viewModel = {
+        locations: locations,
+        ninjas: ninjas,
+        stringifiedLocations: stringifiedLocations
+      };
+      res.render('pirate', { user: req.user, viewModel: viewModel });
     });
   });
 });
@@ -28,7 +29,6 @@ function getFirstNinja(ninjas){
 
 function getLocationsFromNinja(token, firstNinjaId, callback){
   var options = {
-    //url: "http://dev.whereuat.ninja/api/locations/84af3469-d270-407f-8717-01fd9ffac57e",
     url: "http://docker/api/locations/"+firstNinjaId,
     method: 'GET',
     json: true,
