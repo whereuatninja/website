8a0895e1e6561fe3229ddb5db8f4c3ab39a9a319
sdkraemer <tfcmaster9@gmail.com>
2016-05-14 02:22:15 -0700

    Locations should now show for first ninja of pirate(user logged in)

--- routes/pirate.js
+++ routes/pirate.js
@@ -7,13 +7,14 @@ var router = express.Router();
 /* GET user profile. */
 router.get('/', ensureLoggedIn, function(req, res, next) {
   var userId = "87c6673b-ca53-440f-80ea-9c581caa6c1b";
-
+  console.log("user: %j", req.user);
   getUserObj(req.user.token, function(ninjas){
     var firstNinjaId = getFirstNinja(ninjas);
     console.log("firstNinjaId: "+firstNinjaId);
-    getLocationsFromNinja(firstNinjaId, function(locations){
+    getLocationsFromNinja(req.user.token, firstNinjaId, function(locations){
       console.log(locations);
-      res.render('pirate', { user: req.user, locations: locations, ninjas: ninjas});
+      var stringifiedLocations = JSON.stringify(locations);
+      res.render('pirate', { user: req.user, locations: locations, ninjas: ninjas, stringifiedLocations: stringifiedLocations });
     });
   });
 });
@@ -25,11 +26,13 @@ function getFirstNinja(ninjas){
   return undefined;
 }
 
-function getLocationsFromNinja(firstNinjaId, callback){
+function getLocationsFromNinja(token, firstNinjaId, callback){
   var options = {
     //url: "http://dev.whereuat.ninja/api/locations/84af3469-d270-407f-8717-01fd9ffac57e",
-    url: "http://dev.whereuat.ninja/api/locations/"+firstNinjaId,
-    method: 'GET'
+    url: "http://docker/api/locations/"+firstNinjaId,
+    method: 'GET',
+    json: true,
+    auth: { bearer: token }
   };
 
   var requestCallback = function(error, response, body){
@@ -41,13 +44,15 @@ function getLocationsFromNinja(firstNinjaId, callback){
 
 
 function getUserObj(token, callback){
+  console.log("token:"+token);
   var options = {
-    url: "http://node-1:3000/api/ninjas",
+    url: "http://docker/api/ninjas",
     method: 'GET',
     json: true,
     auth: { bearer: token }
   };
   var requestCallback = function(error, response, body){
+    console.log("body: %j", body);
     callback(body);
   };
   request(options, requestCallback);
