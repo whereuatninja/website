3ce44e18aecbd3c437bf8990238ca96b150bc095
Greg Brewer <greg@gabrewer.com>
2016-05-15 13:53:54 -0700

    added Pirate menu item for logged in users

--- routes/about.js
+++ routes/about.js
@@ -1,8 +1,18 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 
+// TODO:  required for signin to work.  need a better way
+var env = {
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+    AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+}
+
 router.get('/', function(req, res, next) {
-    res.render('about', { user: req.user });
+    res.render('about', { user: req.user, env: env });
 });
+
+
 
 module.exports = router;
